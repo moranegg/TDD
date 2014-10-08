b5e89482cda80cffc086b5258abec8f7fcab5870
Morane.Otilia <morane.otilia@gmail.com>
2014-10-08 15:48:30 +0200

    all

--- App/test/firstTest.js
+++ App/test/firstTest.js
@@ -1,7 +1,20 @@
 var should = chai.should();
- 
-describe("Application", function() {
-  it("creates a global variable for the name space", function () {
-    should.exist(todoApp);
+describe('User', function(){
+  describe('#save()', function(){
+    it('should save without error', function(done){
+      var user = new User('Luna');
+      user.save(function(err){
+        if (err) throw err;
+        done();
+      });
+    })
+  })
+})
+describe('Hello World', function(){
+  describe('hello world()', function(){
+    it('hello should start with h', function(){
+      var hello = "Hello world";
+       hello.indexOf(0).should.equal('H');
+    })
   })
 })
