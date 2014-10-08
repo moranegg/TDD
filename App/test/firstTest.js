8f04ff6fe3a5e75f2cebf4f7b1f03bc8ae0797ab
Morane.Otilia <morane.otilia@gmail.com>
2014-10-08 16:43:13 +0200

    clear test

--- App/test/firstTest.js
+++ App/test/firstTest.js
@@ -1,20 +1,11 @@
-var should = chai.should();
-describe('User', function(){
-  describe('#save()', function(){
-    it('should save without error', function(done){
-      var user = new User('Luna');
-      user.save(function(err){
-        if (err) throw err;
+
+describe('pass', function(){
+    it('should pass', function(done){
         done();
-      });
-    })
-  })
+    });
+	 it("should select all dom elements that match hello", function(done){
+	 
+	 });
+	 
 })
-describe('Hello World', function(){
-  describe('hello world()', function(){
-    it('hello should start with h', function(){
-      var hello = "Hello world";
-       hello.indexOf(0).should.equal('H');
-    })
-  })
-})
+
